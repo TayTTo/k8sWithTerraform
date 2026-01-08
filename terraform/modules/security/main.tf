c5f3878aedbaa7a58d96cf1fe39ddf498e570571
TayTTo <doduyanh9803@gmail.com>
2026-01-08 13:35:18 +0700

    commit the first time create ansible and terraform for k8s

--- terraform/modules/security/main.tf
+++ terraform/modules/security/main.tf
@@ -1,7 +1,7 @@
 module "public_subnets_security_group" {
-  source              = "terraform-aws-modules/security-group/aws"
-  name                = "security-group-for-public-subnets"
-  vpc_id              = var.k8s_vpc_id
+  source = "terraform-aws-modules/security-group/aws"
+  name   = "security-group-for-public-subnets"
+  vpc_id = var.k8s_vpc_id
   # ingress_rules       = ["https-443-tcp", "http-80-tcp", "kibana-tcp", "ssh-tcp"]
   ingress_rules       = ["all-all"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
@@ -10,10 +10,14 @@ module "public_subnets_security_group" {
 }
 
 module "private_subnets_security_group" {
-  source                                = "terraform-aws-modules/security-group/aws"
-  name                                  = "security-group-for-private-subnets"
-  vpc_id                                = var.k8s_vpc_id
-  ingress_with_source_security_group_id = [
+  source = "terraform-aws-modules/security-group/aws"
+  name   = "security-group-for-private-subnets"
+  vpc_id = var.k8s_vpc_id
+  ingress_with_source_security_group_id = distinct([
+    {
+      rule                     = "ssh-tcp"
+      source_security_group_id = module.public_subnets_security_group.security_group_id
+    },
     {
       rule                     = "https-443-tcp"
       source_security_group_id = module.public_subnets_security_group.security_group_id
@@ -39,33 +43,33 @@ module "private_subnets_security_group" {
       source_security_group_id = module.public_subnets_security_group.security_group_id
     },
     {
-      from_port = 30000
-      to_port = 32767
-      protocol = "tcp"
-      description = "NodePort Services"
+      from_port                = 30000
+      to_port                  = 32767
+      protocol                 = "tcp"
+      description              = "NodePort Services"
       source_security_group_id = module.private_subnets_security_group.security_group_id
     },
     {
-      from_port = 30000
-      to_port = 32767
-      protocol = "udp"
-      description = "NodePort Services"
+      from_port                = 30000
+      to_port                  = 32767
+      protocol                 = "udp"
+      description              = "NodePort Services"
       source_security_group_id = module.private_subnets_security_group.security_group_id
     },
     {
-      from_port = 10256
-      to_port = 10259
-      protocol = "tcp"
-      description = "k8s network"
+      from_port                = 10256
+      to_port                  = 10259
+      protocol                 = "tcp"
+      description              = "k8s network"
       source_security_group_id = module.private_subnets_security_group.security_group_id
     }
-  ]
-  ingress_with_cidr_blocks = [
-    {
-      rule        = "ssh-tcp"
-      cidr_blocks = "0.0.0.0/0"
-    }
-  ]
+  ])
+  # ingress_with_cidr_blocks = [
+  #   {
+  #     rule        = "ssh-tcp"
+  #     cidr_blocks = "0.0.0.0/0"
+  #   }
+  # ]
   egress_rules       = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
